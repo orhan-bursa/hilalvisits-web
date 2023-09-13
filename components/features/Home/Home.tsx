f294de6329c599bfe755256133115da3c960fcd7
Orhan Bursa <orhan.bursa.arch@gmail.com>
2023-09-14 00:13:01 +0300

    install tailwind, create home about and photos page folders

--- components/features/Home/Home.tsx
+++ components/features/Home/Home.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+    </div>
+  );
 }
--- components/features/index.ts
+++ components/features/index.ts
@@ -1,2 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Blog } from "./Blog"
+export { default as About } from "./About"
+export { default as Photos } from "./Photos"
