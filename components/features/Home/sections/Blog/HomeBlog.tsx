3c21b65d50b168317bc8c299dca76e60d3adffa3
Orhan Bursa <orhan.bursa.arch@gmail.com>
2023-11-28 00:28:28 +0300

    work on homeblog section swiper card ui

--- components/features/Home/sections/Blog/HomeBlog.tsx
+++ components/features/Home/sections/Blog/HomeBlog.tsx
@@ -13,13 +13,14 @@ import "./styles.css";
 // import required modules
 import { Navigation } from "swiper/modules";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomeBlog({ data }: { data: any[] }) {
   return (
     <section className="h-[300px] flex gap-8 flex-wrap md:flex-nowrap">
       <div>
-        <h1 className="text-amber-400 text-[32px] tracking-wider my-4">Destinations</h1>
-        <p className="text-[12px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, quo! Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, quo! Lorem ipsum dolor sit amet.</p>
+        <h1 className="text-amber-400 text-4xl tracking-wider my-4">Destinations</h1>
+        <p className="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, quo! Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, quo! Lorem ipsum dolor sit amet.</p>
         <button className=""></button>
       </div>
       <div className="h-[100%] max-w-2xl border-2 border-amber-200 p-2">
@@ -50,13 +51,25 @@ export default function HomeBlog({ data }: { data: any[] }) {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "flex-end",
-                aspectRatio: 9 / 16
+                aspectRatio: 9 / 16,
               }}
             >
-              <div className="w-[100%] h-[100]">
-                <Image fill src={url} alt={item.title ?? ""} objectFit="cover" />
+              <div className="w-[100%] h-[100%] relative cursor-pointer overflow-hidden child:text-black group">
+                <Link href={"/"}>
+                  <Image fill src={url} alt={item.title ?? ""} objectFit="cover" className="hover:scale-110 transition-all duration-700" />
+                </Link>
+                <div className="absolute -bottom-4 group-hover:bottom-0 duration-500 ">
+                  <Link
+                    href={"/"}
+                  >
+                    <div className="flex flex-col items-start justify-end text-white text-left p-2 pb-3 z-50 w-[100%] h-[50%]">
+                      <h6 className="text-lg font-semibold opacity-80 group-hover:opacity-100">Title Lorem Ipsum dolor sits</h6>
+                      <p className="text-sm opacity-70 group-hover:opacity-100">{"Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, excepturi!"}</p>
+                      <button className="self-end text-xs opacity-0 group-hover:opacity-95">Read more</button>
+                    </div>
+                  </Link>
+                </div>
               </div>
-              <div className="text-white z-50 w-[100%]">Hello</div>
 
             </SwiperSlide>
           }
