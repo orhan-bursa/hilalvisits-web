c8777a1534b2f8e3ddb243ee890f62561cbf3a03
Orhan Bursa <orhan.bursa.arch@gmail.com>
2023-12-09 16:25:46 +0300

    edit homehero section, finish ui design

---
+++ constants/navbar.ts
@@ -0,0 +1,23 @@
+type NavbarItem = { title: string; href: string };
+export const NAVBAR_ITEMS: NavbarItem[] = [
+    {
+        title: "Home",
+        href: "/",
+    },
+    {
+        title: "Blog",
+        href: "/blog",
+    },
+    {
+        title: "Albums",
+        href: "/album",
+    },
+    {
+        title: "Photos",
+        href: "/photo",
+    },
+    {
+        title: "About",
+        href: "/about",
+    },
+];
