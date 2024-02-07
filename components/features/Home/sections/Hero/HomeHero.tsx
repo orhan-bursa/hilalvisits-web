cfe20feca90ea46edae2f80b1da7251533861f2c
Orhan Bursa <orhan.bursa.arch@gmail.com>
2024-02-08 00:33:19 +0300

    rework homehero section, change swiper to show 1 image only, add about me section

--- components/features/Home/sections/Hero/HomeHero.tsx
+++ components/features/Home/sections/Hero/HomeHero.tsx
@@ -17,9 +17,117 @@ import { shortenText } from "@/utils/text";
 import { useBreakpoints } from "@/hooks";
 import { montserrat } from "@/utils/fonts";
 import { BlogPageObject } from "@/types";
-import { destructureBlogProps } from "@/utils";
+import { destructureBlogProps, getUrlFromFilesType } from "@/utils";
+import me from "../../../../../assets/images/v3-nobg.png";
 
-type Socials = {
+export default function HomeHero({ items }: { items: BlogPageObject[] | undefined }) {
+  const { isMobile } = useBreakpoints()
+
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s: any, time: any, progress: any) => {
+    (progressCircle?.current as any)?.style?.setProperty('--progress', 1 - progress);
+    (progressContent?.current as any).textContent = ` · `; //${Math.ceil(time / 1000)}s
+  };
+
+  if (!items || !items.length) return <Alert>Unable to retrieve data from server</Alert>
+
+  return (
+    <section className={cn(
+      "relative cursor-default mx-auto my-8",
+      "w-full h-full max-w-[1200px] md:flex md:gap-8",
+    )}>
+      <div className="w-full h-full md:w-[65%]">
+        <Swiper
+          className="home-hero-swiper"
+          speed={500}
+          slidesPerView={1}
+          spaceBetween={isMobile ? 16 : 32}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Navigation, Pagination, Autoplay]}
+          autoplay={{
+            delay: 5000,
+          }}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          style={{
+            width: "100%",
+            height: "100%",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+
+        >
+          {items.map((item, ind) => {
+            const { cover, title, description } = destructureBlogProps(item)
+            return <SwiperSlide
+              key={ind}
+              style={{
+                textAlign: "center",
+                fontSize: "18px",
+                background: "#fff",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column"
+              }}
+            >
+              <div className="relative h-full min-h-[400px] md:min-h-[600px] w-full">
+                <Image
+                  src={cover ?? ""}
+                  alt={title ?? ""}
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <div className="z-50 w-full top-[60%] absolute bg-black bg-opacity-40 text-white space-x-2">
+                <h2 className="my-2 text-4xl font-[500]">{title ?? "No title"}</h2>
+                <p>{shortenText(description, 100, 15)}</p>
+                <Link href={"/"}>
+                  <button className=" p-1 my-2 border-b-2 border-gray-400 text-lg italic">Read</button>
+                </Link>
+              </div>
+            </SwiperSlide>
+          }
+          )}
+          <div className="autoplay-progress" slot="container-end">
+            <svg viewBox="0 0 48 48" ref={progressCircle}>
+              <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+            <span ref={progressContent}></span>
+          </div>
+        </Swiper>
+      </div>
+      <div className="w-full h-full md:w-[35%] text-center space-y-3 flex gap-4 md:block">
+        <div className="w-full aspect-square relative mx-auto">
+          <Image
+            src={me}
+            alt={"title"}
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="text-start md:text-center">
+          <h1 className="text-2xl font-semibold">Hilal Visits</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit obcaecati voluptatem inventore, minima illum quibusdam, eligendi iste voluptates vitae necessitatibus molestiae culpa quisquam saepe adipisci temporibus maiores, tenetur rerum doloribus?</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+/* <div className="z-50 w-full top-[60%] absolute bg-black bg-opacity-40 text-right text-white space-x-2">
+                <h2 className="my-2 text-4xl font-[500]">{title ?? "No title"}</h2>
+                <p>{shortenText(description, 100, 15)}</p>
+                <Link href={"/"}>
+                  <button className=" p-1 my-2 border-b-2 border-gray-400 text-lg italic">Read</button>
+                </Link>
+              </div> 
+              
+              
+              type Socials = {
   title: string
   href: string
   icon: SvgIconComponent
@@ -41,116 +149,4 @@ const SOCIALS: Socials[] = [
     icon: Facebook,
   },
 ]
-
-export default function HomeHero({ items }: { items: BlogPageObject[] | undefined }) {
-  const { isMobile } = useBreakpoints()
-
-  const progressCircle = useRef(null);
-  const progressContent = useRef(null);
-  const onAutoplayTimeLeft = (s: any, time: any, progress: any) => {
-    (progressCircle?.current as any)?.style?.setProperty('--progress', 1 - progress);
-    (progressContent?.current as any).textContent = ` · `; //${Math.ceil(time / 1000)}s
-  };
-
-  if (!items || !items.length) return <Alert>Unable to retrieve data from server</Alert>
-  return (
-    <section className="w-full h-[900px] sm:h-[800px] md:h-[700px] relative cursor-default">
-      <div className="h-[700px] min-h-[700px] md:h-full w-full bg-gray-200 p-2">
-        <Swiper
-          className="home-hero-swiper"
-          speed={1000}
-          slidesPerView={"auto"}
-          spaceBetween={isMobile ? 8 : 16}
-          initialSlide={1}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          autoplay={{
-            delay: 8000,
-          }}
-          onAutoplayTimeLeft={onAutoplayTimeLeft}
-          style={{
-            width: "100%",
-            height: "100%",
-            position: "relative",
-          }}
-
-        >
-          {items.map((item, ind) => {
-            const { cover, title, description } = destructureBlogProps(item)
-            return <SwiperSlide
-              key={ind}
-              style={{
-                textAlign: "center",
-                fontSize: "18px",
-                background: "#fff",
-                display: "flex",
-                alignItems: "center",
-                width: isMobile ? "100%" : "65%",
-                left: isMobile ? 0 : "35%",
-                position: "relative",
-              }}
-            >
-              <div className="z-50 w-full top-[60%] absolute bg-black bg-opacity-40 text-right text-white space-x-2">
-                <h2 className="my-2 text-4xl font-[500]">{title ?? "No title"}</h2>
-                <p>{shortenText(description, 100, 15)}</p>
-                <Link href={"/"}>
-                  <button className=" p-1 my-2 border-b-2 border-gray-400 text-lg italic">Read</button>
-                </Link>
-
-              </div>
-              <Image
-                src={cover}
-                alt={title ?? ""}
-                fill
-                style={{ objectFit: "cover" }}
-              />
-            </SwiperSlide>
-          }
-          )}
-          <div className="autoplay-progress" slot="container-end">
-            <svg viewBox="0 0 48 48" ref={progressCircle}>
-              <circle cx="24" cy="24" r="20"></circle>
-            </svg>
-            <span ref={progressContent}></span>
-          </div>
-        </Swiper>
-      </div>
-      <div className={cn(
-        "w-full block h-max px-4 md:px-8 py-4 md:mt-2 bg-white z-50 space-y-3 top-0",
-        "md:w-[35%] md:absolute",
-        "opacity-90"
-      )}>
-        <div >
-          <p className="cursor-default">My name is Hilal, I am a traveller, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat accusantium ducimus dignissimos maiores neque sint obcaecati quo dolores quaerat ad.</p>
-        </div>
-        <div className="flex md:flex-col items-center justify-between md:items-start">
-          <Button
-            LinkComponent={Link}
-            href="/"
-            variant="text"
-            color="inherit"
-            sx={{
-              width: 128,
-              marginY: 2,
-              border: "2px solid white",
-              borderBottom: "2px solid #e2e8f0",
-              textTransform: "none",
-              fontFamily: montserrat.className,
-              fontSize: 18,
-              transition: "all 500ms ease",
-              "&:hover": {
-                border: "2px solid #94a3b8"
-              }
-            }}
-          >
-            Read more
-          </Button>
-        </div>
-      </div>
-    </section>
-  );
-}
+              */
