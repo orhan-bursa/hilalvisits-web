24de268ec9acbc96872b387ef51ac9ac0b099304
Orhan Bursa <orhanbursa@siu.com.tr>
2025-11-05 21:26:38 +0300

    some seo work and favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="tr" className={jost.className}>
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+			</head>
 			<body>
 				<div className="relative">
 					<Navbar />
