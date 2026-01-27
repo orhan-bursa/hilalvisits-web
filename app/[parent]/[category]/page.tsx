d63c32e778259c0b279d887207b5852ff63189aa
Orhan Bursa <orhanbursa@siu.com.tr>
2026-01-28 00:00:45 +0300

    remove metadata generate functions to be added later, remove sub category page, move categories to prismic

--- app/[parent]/[category]/page.tsx
+++ app/[parent]/[category]/page.tsx
@@ -3,49 +3,22 @@ import { Category as FeatureCategory } from '@/components'
 import { destructureBlogProps, destructureMenuProps } from '@/utils'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+import prismicClient from '@/lib/prismic'
+import { BlogPageDocument, CategoryPageDocument } from '@/types/prismic-types'
 
 type Props = { params: Promise<{ parent: string; category: string }> }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-	const { category: slug } = await params
-	const blogs = await getBlogs({ menu_slug: slug })
-	const menu = await getMenuBySlug(slug ?? '')
-
-	if (!blogs || blogs.length === 0 || !menu) return {}
-
-	const { title: menuTitle } = destructureMenuProps(menu)
-	const { cover: firstBlogCover, title: firstBlogTitle } = destructureBlogProps(blogs[0])
-
-	const title = `Hilal Visits | ${menuTitle}`
-	const description = `Best blogs and traveling tips for ${menuTitle}`
-	return {
-		title,
-		description,
-		openGraph: {
-			title,
-			description,
-			type: 'website',
-			images: firstBlogCover
-				? [
-						{
-							url: firstBlogCover,
-							width: 384,
-							height: 256,
-							alt: `Cover image of the first blog post: ${firstBlogTitle}`
-						}
-					]
-				: []
-		}
-	}
-}
-
 export default async function Category({ params }: Props) {
 	const { category: slug } = await params
-	const blogs = await getBlogs({ menu_slug: slug })
 
-	if (!blogs?.length) return notFound()
+	const category = await prismicClient
+		.getByUID<CategoryPageDocument>('category', slug)
+		.catch(() => null)
 
-	const menu = await getMenuBySlug(slug ?? '')
+	if (!category) return notFound()
 
-	return <FeatureCategory items={blogs} menu={menu} slug={slug} />
+	const blogs = await prismicClient.getAllByType<BlogPageDocument>('blog')
+	const filteredBlogs = blogs.filter(b => category.data.title === b.data.category.data.title)
+
+	return <FeatureCategory blogs={filteredBlogs} slug={slug} />
 }
