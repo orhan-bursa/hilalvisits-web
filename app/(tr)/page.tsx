c57b56f7af6699630b5607d1e26ff0a7c93b8a0c
Orhan Bursa <orhanbursa@siu.com.tr>
2026-02-06 00:46:28 +0300

    work on metadata, retrieve samples from cms

--- app/(tr)/page.tsx
+++ app/(tr)/page.tsx
@@ -1,6 +1,12 @@
 import HomePageContent from '@/components/features/Home/HomePageContent'
 import { getBlogs } from '@/lib/prismic/services'
-import { NextPage } from 'next'
+import { Metadata, NextPage } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Hilal Visits | Hilalin Seyahat Rehberi',
+	description: 'Türkiye ve dünyadan kareler paylaşan unutulmaz bir gezi rehberi.'
+	//TODO: expand metadata and add meta images / socail media url images
+}
 
 const HomePage: NextPage = async () => {
 	const blogs = await getBlogs('tr')
